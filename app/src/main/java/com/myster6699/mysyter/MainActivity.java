4a3e7071bd9c55c964fa91c9362c48ec943d4b7c
balajis1648 <balaji16021@ece.ssn.edu.in>
2020-05-19 11:49:46 +0530

    Initial commit

---
+++ app/src/main/java/com/myster6699/mysyter/MainActivity.java
@@ -0,0 +1,14 @@
+package com.myster6699.mysyter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
